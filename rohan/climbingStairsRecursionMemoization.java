a8a8f50d143abc23f1ee9ddc3596af8c6cc65823
bharti.r@northeastern.edu <rbharti@athenahealth.com>
2021-09-20 06:45:37 -0400

    climbing stairs

---
+++ rohan/climbingStairsRecursionMemoization.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+/**
+ * Climbing Stairs Recursion solution memoized; top down
+ * Dynamic Programming.
+ */
+class SolutionCSRM {
+    public int climbStairs(int n) {
+        int[] memo = new int[n+1];
+        //specify the base case outside the recursion method
+        memo[0] = 1;
+        memo[1] = 1;
+        
+        return recurse(n, memo);
+    }
+    
+    private int recurse(int n, int[] memo) {
+        // check if memoized solution exists
+        if(memo[n] > 0)
+            return memo[n];
+        
+        // compute the number of ways
+        int numberOfWays = recurse(n-1, memo) + recurse(n-2, memo);
+        memo[n] = numberOfWays;
+        return numberOfWays;
+    }
+}
