015ff47512c95b2e894f0396fc96424d33d6a18c
bharti.r@northeastern.edu <rbharti@athenahealth.com>
2021-09-28 08:12:01 -0400

    house robber

---
+++ DFS/houseRobber.java
@@ -0,0 +1,29 @@
+/**
+ * We start from the 0th house. When at a certain house, we either choose the
+ * next house or the gold from the current and next to next house since we
+ * cannot rob two adjacent houses.
+ * 
+ * Average - O(N) time | O(N) Space
+ */
+class houseRobber {
+    public int rob(int[] nums) {
+        int[] memo = new int[nums.length];
+        Arrays.fill(memo, -1);
+
+        return robFrom(0, nums, memo);
+    }
+
+    private int robFrom(int house, int[] nums, int[] memo) {
+        if (house >= nums.length) {
+            return 0;
+        }
+
+        if (memo[house] > -1)
+            return memo[house];
+
+        int result = Math.max(robFrom(house + 1, nums, memo), robFrom(house + 2, nums, memo) + nums[house]);
+
+        memo[house] = result;
+        return result;
+    }
+}
---
+++ DP/houseRobberDP.java
@@ -0,0 +1,29 @@
+/**
+ * We start from the 0th house. When at a certain house, we either choose the
+ * next house or the gold from the current and next to next house since we
+ * cannot rob two adjacent houses.
+ * 
+ * Average - O(N) time | O(N) Space
+ */
+class houseRobberDP {
+    public int rob(int[] nums) {
+        if (nums.length == 0 || nums == null)
+            return 0;
+
+        if (nums.length == 1)
+            return nums[0];
+
+        if (nums.length == 2)
+            return Math.max(nums[0], nums[1]);
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+
+        return dp[nums.length - 1];
+    }
+}
