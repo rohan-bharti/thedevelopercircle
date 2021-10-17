8a203f70b3e8b55e757bd3071edd00e4d58a6e43
Rohan Bharti <rbharti@athenahealth.com>
2021-10-17 07:02:08 -0400

    longest common subsequence

--- DP/longestCommonSubsequence.java
+++ DP/longestCommonSubsequence.java
@@ -1,6 +1,6 @@
 /**
  * Longest Common Subsequence. The idea is to compare smaller portions of the two strings to create a memoized table to further grow towards
- * the bigger problems.
+ * the bigger problems. Bottom Up.
  *
  * Time - O(M.N) | Space - O(M.N)
  */
@@ -31,5 +31,37 @@ class Solution {
         }
         
         return dp[m][n]; 
+    }
+}
+
+/**
+ * Longest Common Subsequence. The idea is to compare smaller portions of the two strings to create a memoized table to further grow towards
+ * the bigger problems. Top Down.
+ *
+ * Time - O(M.N) | Space - O(M.N)
+ */
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        
+        Integer[][] dp = new Integer[m+1][n+1];
+        
+        return lcs(m, n, dp, text1, text2);
+    }
+    
+    private int lcs(int i, int j, Integer[][] cache, String s1, String s2) {
+        if( i==0 || j==0)
+            return 0;
+        
+        if( cache[i][j] != null)
+            return cache[i][j];
+        
+        if(s1.charAt(i-1) == s2.charAt(j-1))
+            cache[i][j] = 1 + lcs(i-1, j-1, cache, s1, s2);
+        else
+            cache[i][j] = Math.max(lcs(i-1, j, cache, s1, s2), lcs(i, j-1, cache, s1, s2));
+        
+        return cache[i][j];
     }
 }
