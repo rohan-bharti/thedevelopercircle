644e4bad59cc121ec29ba5148f55ffd8a1d23fc0
Rohan Bharti <rbharti@athenahealth.com>
2021-10-17 10:05:55 -0400

    house robber correction

--- DP/houseRobberDP.java
+++ DP/houseRobberDP.java
@@ -17,7 +17,7 @@ class houseRobberDP {
             return Math.max(nums[0], nums[1]);
 
         int[] dp = new int[nums.length];
-        dp[0] = nums[0];
+        dp[0] = nums[0]; // amount of money robbing 0th house
         dp[1] = Math.max(nums[0], nums[1]);
 
         for (int i = 2; i < dp.length; i++) {
